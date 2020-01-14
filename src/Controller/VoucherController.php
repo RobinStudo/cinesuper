45a9617a372d1fb50cda84f58066afed02421bcc
sahbia <ouaja.sahbia@gmail.com>
2020-01-14 11:30:40 +0100

    generate voucher

--- src/Controller/VoucherController.php
+++ src/Controller/VoucherController.php
@@ -15,65 +15,44 @@ class VoucherController extends AbstractController
      */
     public function vouchergenerate(Card $card)
     {
-          $voucher = new Voucher();
+          if($card->getFidelity() >=10) 
+         { 
+            $nbplace=$card->getFidelity();
+            $nbVoucher= intdiv($nbplace,10);
+            for ($i=1; $i <= $nbVoucher; $i++)
+             { 
+             
 
-          $voucher->setCard($card);
-          
-
-          $serialNumber =  time() . mt_rand( 10000, 99999); 
-
-          $voucher->setSerial($serialNumber);
-
-          $voucher->setExpiredAt(new \DateTime( '6 months' ));
-
-          // add voucher to Card
-          $card->addVoucher($voucher);  
-
-          $em = $this->getDoctrine()->getManager();
-          $em->persist($voucher);
-          $em->flush();
-
-          $this->addFlash("success", "le bon de fideleté est generer avec succee ");
-
+                $voucher = new Voucher();
+            
+                $voucher->setCard($card);
+                
+    
+                $serialNumber = mt_rand( 1000, 99999); 
+    
+                $voucher->setSerial($serialNumber);
+    
+                $voucher->setExpiredAt(new \DateTime( '6 months' ));
+    
+                // add voucher to Card
+                $card->addVoucher($voucher);  
+    
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($voucher);
+                $em->persist($card);
+                $em->flush();
+    
+                $this->addFlash("success", "le bon de fideleté est generer avec succee ");
+            }
+             
+        }
           return $this->redirectToRoute('dashboard') ;
        
     }
 
-    /**
-     * @Route("/vouchers/display/{id}", name="vouchers")
-     */
-    public function diplayAllvouchers(Card $card)
-    {
-        $vouchers = $card->getVouchers();
+    
 
-        return $this->render('dashbord',[
-
-            'vouchers' =>$vouchers
-        ]);
-
-    }
-
-    /**
-     * @Route("/vouchers/delete/{id}", name="delete_voucher")
-     */
-    public function deletevoucher(Voucher $voucher)
-    {
-        $card =$voucher->getCard();
-
-        $entityManager = $this->getDoctrine()->getManager();
-
-        $entityManager->remove($voucher);
-
-        //delete voucher in card 
-
-        $card ->removeVoucher($voucher);
-
-        $entityManager->flush();
-
-
-        return $this->render('dashbord');
-
-    }
+   
 
 
 
